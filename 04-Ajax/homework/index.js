a9bdc3e8ebf13b91af4e30c8229c9cf30228afe8
Juan José Martinez <juanjomm71@gmail.com>
2022-08-09 19:38:40 -0300

    homework finished

--- 04-Ajax/homework/index.js
+++ 04-Ajax/homework/index.js
@@ -1,6 +1,26 @@
-$("#boton").click(function () {
-    $.get("http://localhost:5000/amigos", function (data) {
-        var amigo = document.createElement("li");
-        amigo.src = data.url;
-    }
-})
+$("#boton").click(() => {
+    let lista = $("#lista");
+    lista.empty();
+    $.get("http://localhost:5000/amigos", (friends) => {
+        friends.forEach((friend) => lista.append(`<li>${friend.name}</li>`));
+    });
+});
+
+$("#search").click(() => {
+    let id = $("#input").val();
+    $.get(`http://localhost:5000/amigos${id}`, (friend) => {
+        $("#amigo").text(friend.name);
+    });
+});
+
+$("#delete").click(() => {
+    let id = $("#inputDelete").val();
+    $.ajax({
+        url: `http://localhost:5000/amigos${id}`,
+        method: "DELETE",
+        succes: () => {
+            $("#succes").text(`Amigo ${id} eliminado con éxito`)
+        }
+    });
+});
+
--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 
-export default function Card(props) {
+export default function Card({ max, min, name, img, onClose }) {
   // acá va tu código
-  return <div>Card Component</div>
+  return (
+    <div>
+      <button onClick={onClose}>X</button>
+      <h2>{name}</h2>
+      <div>
+        <h3>Min</h3>
+        <h3>{min}</h3>
+        <h3>Max</h3>
+        <h3>{max}</h3>
+      </div>
+      <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt={"icon"} />
+    </div>
+  );
 };
